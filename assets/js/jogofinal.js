f17d60b2eab71f36dcc3070f695816018909a908
Fred <89613131+FredRCP@users.noreply.github.com>
2023-02-15 18:57:27 -0300

    Add files via upload

--- assets/js/jogofinal.js
+++ assets/js/jogofinal.js
@@ -14,6 +14,7 @@ audio.onended = function(){
 
 //CRIAÇÃO DA CLASSE DOS JOGADORES
 
+let iniciar=0;
 let nudez=0;
 let transa=0;
 class Jogador{
@@ -31,8 +32,8 @@ class Jogador{
         else{
             let num1= Math.floor(Math.random()*34);
             this.contador+=1;
-            //if(this.contador%3===0){this.perguntar};
-            //if(this.contador%6===0){this.beber};
+            if(this.contador%3===0){this.perguntar};
+            if(this.contador%6===0){this.beber};
             if(num1===33&&this.pecas>0){let missao= corpoh[num1]; this.retirarpecam; return missao} 
             else{let missao= corpoh[num1]; return missao};}
     };
@@ -42,8 +43,8 @@ class Jogador{
         else{
             let num1= Math.floor(Math.random()*34);
             this.contador+=1;
-            //if(this.contador%3===0){this.perguntar};
-            //if(this.contador%6===0){this.beber};
+            if(this.contador%3===0){this.perguntar};
+            if(this.contador%6===0){this.beber};
             if(num1===33&&this.pecas>0){let missao= corpom[num1]; this.retirarpecah; return missao} 
             else{let missao= corpom[num1]; return missao};}
     };
@@ -52,8 +53,7 @@ class Jogador{
         let num= Math.floor(Math.random()*7);
         let missao= acaom2[num];
         transa+=1;
-        if(transa>=10){alert("FIM DE JOGO! HORA DE TRANSAR!!!"); document.querySelector('#output').innerHTML="";
-        return document.querySelector('#output').innerHTML= homem.nome + " & " + mulher.nome + "<br>" + "FIM DE JOGO! HORA DE TRANSAR!!!"};
+        if(transa>=10){alert("FIM DE JOGO! HORA DE TRANSAR!!!"); return missao= " & " + homem.nome + "<br>" + "FIM DE JOGO! HORA DE TRANSAR!!!"};
         return missao;
     };
 
@@ -61,18 +61,17 @@ class Jogador{
         let num= Math.floor(Math.random()*7);
         let missao= acaoh2[num];
         transa+=1;
-        if(transa>=10){alert("FIM DE JOGO! HORA DE TRANSAR!!!"); document.querySelector('#output').innerHTML="";
-        return document.querySelector('#output').innerHTML=homem.nome + " & " + mulher.nome + "<br>" + "FIM DE JOGO! HORA DE TRANSAR!!!"};
+        if(transa>=10){alert("FIM DE JOGO! HORA DE TRANSAR!!!"); return missao= " & " + mulher.nome + "<br>" + "FIM DE JOGO! HORA DE TRANSAR!!!"};
         return missao;
     };
 
     get perguntar(){
-        
-        let numero= Math.floor(Math.random()*40);
-        if(this.lista.indexOf(perguntas[numero])===-1){
-        this.lista.push(perguntas[numero]);
-        console.log(perguntas[numero]);} else(alert("pergunte o que quiser ao seu parceiro(a)"));
-    }
+        if(document.querySelector('#perguntas').checked===true){
+            let numero= Math.floor(Math.random()*40);
+            if(this.lista.indexOf(perguntas[numero])===-1){
+            this.lista.push(perguntas[numero]);
+            console.log(perguntas[numero]);} else(alert("pergunte o que quiser ao seu parceiro(a)"));
+    }}
 
     get listar(){
         console.log(this.lista);
@@ -99,13 +98,24 @@ class Jogador{
     };
 
     get beber(){
-        alert('tome um gole');
+        if(document.querySelector('#drink').checked===true){
+        alert('tome um gole do seu drink');
+    }}
+
+    get listarroupasm(){
+        let armariom="";
+        for(let i in this.roupas){
+            armariom+="peça "+ (Number(i)+1) + ": " + this.roupas[i] + "<br>";
+        }
+        document.querySelector(".armariom").innerHTML= armariom;
     }
 
-    get listarroupas(){
+    get listarroupash(){
+        let armarioh="";
         for(let i in this.roupas){
-            "peça "+ (Number(i)+1) + ": " + this.roupas[i];
+            armarioh+="peça "+ (Number(i)+1) + ": " + this.roupas[i] + "<br>";
         }
+        document.querySelector(".armarioh").innerHTML= armarioh;
     }
 }
 
@@ -115,28 +125,26 @@ class Jogador{
 
 //DADOS FASE 1
 //34
-const corpom=["toque os lábios", "beije os lábios", "mordisque os lábios", "dê uma lambida no cantinho dos lábios", "dê um beijão de cinema", "assopre os lábios", "massageie as orelhas", "dê mordidinhas nas orelhas", 
-"assopre as orelhas", "dê lambidas nas orelhas", "massageie a nuca", "arranhe levemente a nuca", "dê vários beijos na nuca", "assopre e mordisque a nuca", "dê beijos no pescoço", 
-"dê lambidas e leves chupadas no pescoço", "acaricie os seios", "apalpe os seios", "massageia as costas", 
-"se ela estiver de roupas íntimas: beije, dê lambidas e assopre as costas, em toda a sua extensão, senão role o dado novamente", "beije todo o abdome", " deslize as mãos suavemente por todo o abdome", "dê mordidinhas no abdome",
-"massageie as coxas", "deslize as mãos pela partes internas das coxas, arranhando levemente de baixo pra cima", "beije a parte interna das coxas; comece próximo aos joelhos e suba até a virilha",
-"se ela estiver apenas com roupa íntima, dê lambidinhas na virilha, senão role o dado novamente", "apalpe e massageie a bunda", "se ela estiver apenas com roupa íntima, dê mordidinhas na bunda, senão role o dado novamente",
-"a escolha é livre, faça o quiser!", "se ela estiver apenas com roupas debaixo, toque as partes íntimas, senão role o dado novamente",
+const corpom=["toque os lábios dela", "beije os lábios dela", "mordisque os lábios dela", "dê uma lambida no cantinho da boca dela", "dêem um beijão de cinema", "assopre os lábios dela", "massageie as orelhas dela", "dê mordidinhas nas orelhas dela", 
+"assopre as orelhas dela", "dê lambidas nas orelhas dela", "massageie a nuca dela", "arranhe levemente a nuca dela", "dê vários beijos na nuca dela", "assopre e mordisque a nuca dela", "beije o pescoço dela", 
+"dê lambidas e leves chupadas no pescoço dela", "acaricie os seios dela", "apalpe os seios dela", "massageia as costas dela", 
+"se ela estiver de roupas íntimas: beije, dê lambidas e assopre suas costas, em toda a sua extensão, senão role o dado novamente", "beije todo o abdome dela", " deslize as mãos, suavemente, por todo o abdome dela", "dê mordidinhas no abdome dela",
+"massageie as coxas dela", "deslize as mãos pela parte interna das coxas dela, arranhando levemente de baixo pra cima", "beije a parte interna das coxas dela; inicie próximo aos joelhos e suba até a virilha",
+"se ela estiver apenas com roupa íntima, dê lambidinhas na virilha, senão role o dado novamente", "apalpe e massageie a bunda dela", "se ela estiver apenas com roupa íntima, dê mordidinhas na bunda dela, senão role o dado novamente",
+"a escolha é livre, faça o quiser com ela!", "se ela estiver apenas com roupas debaixo, toque as partes íntimas, senão role o dado novamente",
 "se ela estiver apenas com roupas debaixo, toque e acaricie as partes íntimas, senão role o dado novamente",
 "se ela estiver apenas com roupas debaixo, dê vários beijos nas partes íntimas (por cima da roupa), senão role o dado novamente", "retire uma peça de roupa!"];
 //34
-const corpoh=["toque os lábios", "beije os lábios", "mordisque os lábios", "dê uma lambida no cantinho dos lábios", "dê um beijão de cinema", "assopre os lábios", "massageie as orelhas", "dê mordidinhas nas orelhas", 
-"assopre as orelhas", "dê lambidas nas orelhas", "massageie a nuca", "arranhe levemente a nuca", "dê vários beijos na nuca", "assopre e mordisque a nuca", "dê beijos no pescoço", 
-"dê lambidas e leves chupadas no pescoço", "acaricie o peito", "apalpe o peito", "massageia as costas", 
-"se ele estiver de roupas íntimas: beije, dê lambidas e assopre as costas, em toda a sua extensão, senão role o dado novamente", "beije todo o abdome", " deslize as mãos suavemente por todo o abdome", "dê mordidinhas no abdome",
-"massageie as coxas", "deslize as mãos pela partes internas das coxas, arranhando levemente de baixo pra cima", "beije a parte interna das coxas; comece próximo aos joelhos e suba até a virilha",
-"se ele estiver apenas com roupa íntima, dê lambidinhas na virilha, senão role o dado novamente", "apalpe e massageie a bunda", "se ele estiver apenas com roupa íntima, dê mordidinhas na bunda, senão role o dado novamente",
-"a escolha é livre, faça o quiser!", "se ele estiver apenas com roupas debaixo, toque as partes íntimas, senão role o dado novamente",
+const corpoh=["toque os lábios dele", "beije os lábios dele", "mordisque os lábios dele", "dê uma lambida no cantinho da boca dele", "dêem um beijão de cinema", "assopre os lábios dele", "massageie as orelhas dele", "dê mordidinhas nas orelhas dele", 
+"assopre as orelhas dele", "dê lambidas nas orelhas dele", "massageie a nuca dele", "arranhe levemente a nuca dele", "dê vários beijos na nuca dele", "assopre e mordisque a nuca dele", "dê beijos no pescoço dele", 
+"dê lambidas e leves chupadas no pescoço dele", "acaricie o peito dele", "apalpe o peito dele", "massageia as costas dele", 
+"se ele estiver de roupas íntimas: beije, dê lambidas e assopre as costas, em toda a sua extensão, senão role o dado novamente", "beije todo o abdome dele", " deslize as mãos, suavemente, por todo o abdome dele", "dê mordidinhas no abdome dele",
+"massageie as coxas dele", "deslize as mãos pela parte interna das coxas dele, arranhando levemente de baixo pra cima", "beije a parte interna das coxas dele; inicie próximo aos joelhos e suba até a virilha",
+"se ele estiver apenas com roupa íntima, dê lambidinhas na virilha dele, senão role o dado novamente", "apalpe e massageie a bunda dele", "se ele estiver apenas com roupa íntima, dê mordidinhas na bunda dele, senão role o dado novamente",
+"a escolha é livre, faça o quiser com ele!", "se ele estiver apenas com roupas debaixo, toque as partes íntimas, senão role o dado novamente",
 "se ele estiver apenas com roupas debaixo, toque e acaricie, apalpe as partes íntimas, senão role o dado novamente",
 "se ele estiver apenas com roupas debaixo, dê vários beijos nas partes íntimas (por cima da roupa), senão role o dado novamente", "retire uma peça de roupa!"];
 
-//const acao=["acaricie ", "apalpe/toque/massageie ", "beije ", "dê uma lambida n", "mordisque ", "use sua imaginação com ", "arranhe de leve ",
-// "acaricie ", "apalpe/toque ", "beije ", "assopre ", "tire uma peça de roupa"];
 
 //DADOS FASE 2
 //7
@@ -164,17 +172,20 @@ const perguntas=['descreva sua lingerie/roupa debaixo','qual a cor preferida de 
  'sussurre uma sacanagem no ouvido do seu parceiro(a)'];
 
 function apelar(){
-    if(mulher.pecas>0){mulher.retirarpecam};
-    if(homem.pecas>0){homem.retirarpecah};
+    if(iniciar===0){return alert('você precisa iniciar o jogo!')};
+    if(mulher.pecas>0){mulher.retirarpecam; mulher.listarroupasm}
+    if(homem.pecas>0){homem.retirarpecah; homem.listarroupash;}
     alert("Ambos tirem uma peça de roupa!")
 };
 
 function resetar(){
+    iniciar=1;
     nudez=0;
     transa=0;
     //CRIAÇÃO DOS JOGADORES
     jog1= prompt("Qual o nome da jogadora?") || "Jogadora 1";
-    jog1p= Number(prompt("Quantas peças de roupa está usando?"));
+    jog1p= Number(prompt("Quantas peças de roupa está usando? Não inclua os calçados."));
+    alert("A seguir, liste primeiro as peças íntimas.");
     roupas1=[];
     for(i=0; i<jog1p; i++){
     roupas1[i]= prompt( 'Qual a peça número ' + (i+1) + '?');
@@ -183,8 +194,9 @@ function resetar(){
     bebida1=0;
     contador1=0;
     jog2= prompt("Qual o nome do jogador?") || "Jogador 2";
-    jog2p= Number(prompt("Quantas peças de roupa está usando?"));
+    jog2p= Number(prompt("Quantas peças de roupa está usando? Não inclua os calçados."));
     roupas2=[];
+    alert("A seguir, liste primeiro as peças íntimas.");
     for(i=0; i<jog2p; i++){
     roupas2[i]= prompt( 'Qual a peça número ' + (i+1) + '?');
     };
@@ -195,19 +207,21 @@ function resetar(){
     homem= new Jogador(jog2, jog2p, roupas2, lista2, bebida2, contador2);
     document.querySelector(".jogador1").innerHTML=mulher.nome;
     document.querySelector(".jogador2").innerHTML=homem.nome;
-    
-   
+    mulher.listarroupasm;
+    homem.listarroupash;
+    document.querySelector('.resultadojogo').innerHTML="";   
 };
 
-
 function dadoele(){
-    document.querySelector('.resultadojogo').innerHTML= homem.nome +":"+"<br>" +homem.sortear1h;
-   // document.querySelector("#cara").innerHTML= homem.roupas;
+    if(iniciar===0){return alert('você precisa iniciar o jogo!')};
+    document.querySelector('.resultadojogo').innerHTML= homem.nome +"<br>" +homem.sortear1h;
+    
 };
 
 function dadoela(){
-    document.querySelector('.resultadojogo').innerHTML=mulher.nome + ":"+"<br>"  +mulher.sortear1m;
-    mulher.roupas;
+    if(iniciar===0){return alert('você precisa iniciar o jogo!')};
+    document.querySelector('.resultadojogo').innerHTML=mulher.nome +"<br>"  +mulher.sortear1m;
+    
 };
 
 
